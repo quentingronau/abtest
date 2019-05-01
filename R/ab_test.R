d8d02d244b5a6905902fc860ac7bbecd5eae1c83
QUENTIN <quentin.f.gronau@gmail.com>
2019-05-01 13:35:43 +0200

    added link

--- R/ab_test.R
+++ R/ab_test.R
@@ -140,7 +140,7 @@
 #' @references Kass, R. E., & Vaidyanathan, S. K. (1992). Approximate Bayes
 #'   factors and orthogonal parameters, with application to testing equality of
 #'   two binomial proportions. \emph{Journal of the Royal Statistical Society,
-#'   Series B, 54}, 129-144. <doi:10.1111/j.2517-6161.1992.tb01868.x.>
+#'   Series B, 54}, 129-144. \url{https://doi.org/10.1111/j.2517-6161.1992.tb01868.x}
 #' @example examples/example.ab_test.R
 #'
 #' @seealso \code{\link{elicit_prior}} allows the user to elicit a prior based
